89e5923df18506d9396bafde851e696ae05bc879
WinHtetZaw <130633783+WinHtetZaw@users.noreply.github.com>
2026-05-18 09:52:11 +0630

    style(ui): add glass card component

---
+++ src/components/ui/glass-card.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utills";
+
+export function GlassCard({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "rounded-[32px] border border-white/10 bg-white/5 backdrop-blur-2xl",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
