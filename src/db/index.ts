2a9b72ca11e5c4b9220d8578bbade916f79affef
WinHtetZaw <130633783+WinHtetZaw@users.noreply.github.com>
2026-05-19 09:41:03 +0630

    chore(DB): change neon http to serveless

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,13 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { Pool } from "@neondatabase/serverless";
 import * as schema from "./schema";
 import { env } from "@/data/env/server";
 
-const sql = neon(env.DATABASE_URL);
-// export const db = drizzle({ client: sql });
-export const db = drizzle(sql, { schema, logger: true });
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, {
+  schema,
+  logger: true,
+});
