3e8cede88dba4b029383710c29b346faa372cbbc
WinHtetZaw <130633783+WinHtetZaw@users.noreply.github.com>
2026-06-14 23:52:34 +0630

    feat(utils): add handleToast and pluralize utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,3 +12,21 @@ export function stringEquals(a: string, b: string) {
 
 export const wait = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const handleToast = (success: boolean, message: string) => {
+  if (!success) {
+    toast.error(message ?? "Something wrong!");
+    return;
+  }
+  toast.success(message ?? "Success");
+};
+
+export const pluralize = (
+  count: number | string,
+  singular: string,
+  plural?: string,
+) => {
+  if (typeof count === "string") count = +count;
+  if (count === 1) return `${count} ${singular}`;
+  return `${count} ${plural || singular + "s"}`;
+};
